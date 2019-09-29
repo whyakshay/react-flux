4ae8068aa0f5374a64502f95bd18bcc984733427
Akshay Arora <akshay_arora38@optum.com>
2019-09-30 00:28:58 +0530

    Add Actions
    Author:    Akshay Arora <ak_arora@hotmail.com>

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,21 +1,32 @@
 import dispatcher from '../appDispatcher';
-import * as courseApi form '../api/courseApi';
-import actionTypes from '../actionTypes';
+import * as courseApi from '../api/courseApi';
+import actionTypes from './actionTypes';
 
 export function loadCourses(){
-
+  return courseApi.getCourses().then(courses => {
+    dispatcher.dispatch({
+      actionType: actionTypes.LOAD_COURSES,
+      courses: courses
+    });
+  });
 };
 
 // saveCourse Action creator
 export function saveCourse(course){
   return courseApi.saveCourse(course).then((savedCourse) => {
+    console.log(savedCourse);
     dispatcher.dispatch({
       actionType: actionTypes.CREATE_COURSE,
-      course: saveCourse
+      course: savedCourse
     }) 
   }); 
 };
 
 export function deleteCourse(id){
-
+  return courseApi.deleteCourse(id).then(() => {
+    dispatcher.dispatch({
+      actionType: actionTypes.DELETE_COURSE,
+      courseId: id
+    });
+  });
 };
